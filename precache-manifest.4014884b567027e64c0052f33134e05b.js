e6148d879c1ca306064417a4c5ffa71355b71223
Nocelli <rafaelnocelliifsp@gmail.com>
2020-03-22 01:26:58 -0300

    Updates

---
+++ precache-manifest.4014884b567027e64c0052f33134e05b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f143a97601419bbacbb0acde4d4b8a",
+    "url": "/pokedexapp/index.html"
+  },
+  {
+    "revision": "b9008bf5df80e070b4df",
+    "url": "/pokedexapp/static/css/main.414ad27a.chunk.css"
+  },
+  {
+    "revision": "52f777a4619317fc7477",
+    "url": "/pokedexapp/static/js/2.0e5db4a4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/pokedexapp/static/js/2.0e5db4a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9008bf5df80e070b4df",
+    "url": "/pokedexapp/static/js/main.f10a8fa2.chunk.js"
+  },
+  {
+    "revision": "2977afb6372e82ce4201",
+    "url": "/pokedexapp/static/js/runtime-main.c5572ef5.js"
+  }
+]);
